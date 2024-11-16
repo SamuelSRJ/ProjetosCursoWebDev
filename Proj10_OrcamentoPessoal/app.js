20632f09089bfb191084078b54bd357be6c3d249
Samuel de Souza <samuelsouzarosajr@gmail.com>
2024-11-16 06:07:17 -0300

    Commit - Versão final do projeto Controle de Despesas

--- Proj10_OrcamentoPessoal/app.js
+++ Proj10_OrcamentoPessoal/app.js
@@ -56,10 +56,57 @@ class Db {
                 continue // O "continue" ignora todos os comandos restantes e da sequencia no laço
             }
 
+            despesa.id = i
             despesas.push(despesa)
         }
 
         return despesas
+    }
+
+    pesquisar(despesa) {
+        let despesasFiltradas = Array()
+
+        despesasFiltradas = this.recuperarTodosRegistros()
+
+        console.log(despesa)
+        console.log(despesasFiltradas)
+
+        // Ano
+        if(despesa.ano != '') {
+            console.log('filtro ano')
+            despesasFiltradas = despesasFiltradas.filter(d => d.ano == despesa.ano)
+        }
+        // Mes
+        if (despesa.mes != '') {
+            console.log('filtro mes')
+            despesasFiltradas = despesasFiltradas.filter(d => d.mes == despesa.mes);
+        }
+        // Dia
+        if(despesa.dia != '') {
+            console.log('filtro dia')
+            despesasFiltradas = despesasFiltradas.filter(d => d.dia == despesa.dia)
+        }
+        // Tipo
+        if(despesa.tipo != '') {
+            console.log('filtro tipo')
+            despesasFiltradas = despesasFiltradas.filter(d => d.tipo == despesa.tipo)
+        }
+        // Descricao
+        if(despesa.descricao != '') {
+            console.log('filtro descricao')
+            despesasFiltradas = despesasFiltradas.filter(d => d.descricao == despesa.descricao)
+        }
+        // Valor
+        if(despesa.valor != '') {
+            console.log('filtro valor')
+            despesasFiltradas = despesasFiltradas.filter(d => d.valor == despesa.valor)
+        }
+
+        return despesasFiltradas
+    }
+
+    remover(id) {
+        localStorage.removeItem(id)
     }
 }
 
@@ -86,6 +133,14 @@ function cadastrarDespesa() {
 
         // Dialog de sucesso
         $('#modalRegistraDespesa').modal('show')
+        
+        ano.selectedIndex = 0
+        mes.selectedIndex = 0
+        dia.value = ''
+        tipo.selectedIndex = 0
+        descricao.value = ''
+        valor.value = ''
+
     } else {
         document.getElementById('modal_titulo_div').className = 'modal-header text-danger'
         document.getElementById('modal_titulo').innerHTML = 'Erro na inclusão do registro'
@@ -98,12 +153,14 @@ function cadastrarDespesa() {
     }
 }
 
-function carregaListaDespesas() {
-    let despesas = Array()
+function carregaListaDespesas(despesas = Array(), filtro = false) {
 
-    despesas = db.recuperarTodosRegistros()
+    if(despesas.length == 0 && filtro == false) {
+        despesas = db.recuperarTodosRegistros()
+    }
 
     let listaDespesas = document.getElementById('listaDespesas')
+    listaDespesas.innerHTML = ''
 
     // <tr>
     //     <td>15/03/2018</td>
@@ -136,9 +193,37 @@ function carregaListaDespesas() {
         }
 
         linha.insertCell(1).innerHTML = d.tipo
-
-
         linha.insertCell(2).innerHTML = d.descricao
         linha.insertCell(3).innerHTML = d.valor
+
+        // Cria o botão de exclusão
+        let btn = document.createElement('button')
+        btn.className = 'btn btn-danger'
+        btn.innerHTML = '<i class="fas fa-times"></i>'
+        btn.id = `id_despesa_${d.id}`
+        btn.onclick = function() {
+            // Remover despesa
+            let id = this.id.replace('id_despesa_', '')
+            // alert(id)
+            db.remover(id)
+            window.location.reload()
+        }
+        linha.insertCell(4).append(btn)
+
     })
+}
+
+function pesquisarDespesa() {
+    let ano = document.getElementById('ano').value
+    let mes = document.getElementById('mes').value
+    let dia = document.getElementById('dia').value
+    let tipo = document.getElementById('tipo').value
+    let descricao = document.getElementById('descricao').value
+    let valor = document.getElementById('valor').value
+
+    let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
+
+    let despesas = db.pesquisar(despesa)
+
+    carregaListaDespesas(despesas, true)
 }
