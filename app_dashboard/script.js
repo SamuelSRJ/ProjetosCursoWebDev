6c03709ef4b10cc4d840aaf1e556bdea0723e05c
Samuel de Souza <samuelsouzarosajr@gmail.com>
2025-01-14 11:08:04 -0300

    Commit - Versão final do projeto dashboard

--- app_dashboard/script.js
+++ app_dashboard/script.js
@@ -22,4 +22,25 @@ $(document).ready(() => {
         })
     })
 
+    // AJAX
+    $('#competencia').on('change', e =>{
+        
+        let competencia = $(e.target).val()
+
+        // Método, URL, dados, sucesso, erro
+        $.ajax({
+            type: 'GET',
+            url: 'app.php',
+            data: `competencia=${competencia}`, // x-www-form-trlencoded
+            dataType: 'json',
+            success: dados => {
+                $('#numeroVendas').html(dados.numeroVendas)
+                $('#totalVendas').html(dados.totalVendas)
+            },
+            error: erro => {console.log(erro)}
+        })
+
+        
+    })
+
 })
